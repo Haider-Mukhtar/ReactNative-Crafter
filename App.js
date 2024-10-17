c3074b2cdfc187cb67950201114847948c9d5f7f
Haider Mukhtar <haidermukhtarch405@gmail.com>
2024-10-17 12:17:06 +0500

    RNCrafter v0.0.2
    Basic knowledge of JSX

--- App.js
+++ App.js
@@ -2,9 +2,24 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const App = () => {
+  const framework="React Native";
+  let email="reactnative@gmail.com";
+  var age=23;
+  function name () {
+    return "Haider Mukhtar";
+  }
   return (
     <View>
-      <Text>Ap</Text>
+      <Text style={{fontSize:30, fontStyle:'italic'}}>hello react native app.</Text>
+      <Text style={{fontSize:30, fontStyle:'italic'}}>{framework}</Text>
+      <Text style={{fontSize:30, fontStyle:'italic'}}>{email}</Text>
+      <Text style={{fontSize:30, fontStyle:'italic'}}>{age}</Text>
+      <Text style={{fontSize:30, fontStyle:'italic'}}>{name()}</Text>
+      <Text style={{fontSize:30, fontStyle:'italic'}}>{100+20}</Text>
+      <Text style={{fontSize:30, fontStyle:'italic'}}>{100*20}</Text>
+      <Text style={{fontSize:30}}>{age == 20 ? "Age is above 20" : "Age unknow"}</Text>
+
+
     </View>
   )
 }
